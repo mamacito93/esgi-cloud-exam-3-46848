c34af01266a65c3885f32b1726c2a65040d8585b
mamacito93 <vellencunee@gmail.com>
2023-12-15 11:37:35 +0100

    push

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   s3: {
-    accessKeyId: '', // TODO
-    secretAccessKey: '', // TODO
-    endpoint: '', // TODO
-    bucket: '', // TODO
+    accessKeyId: '0Q3HMDFZX3UN7B2STH6T', // TODO
+    secretAccessKey: 'zmZeJytEcFrwG1OnMnWu5myv887bgVYZihzm1EZS', // TODO
+    endpoint: 's3.eu-west-2.wasabisys.com/esgi-cloud-exam-bucket-46848/image.jpg', // TODO
+    bucket: 'esgi-cloud-exam-bucket-46848', // TODO
   },
   suffix: {
     small: '_small.jpg',
